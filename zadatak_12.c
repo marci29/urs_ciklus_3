9d1d2ef6999e394988c98968a491e3c6d60a15c7
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-03 17:29:04 +0200

    Create zadatak_12.c

---
+++ zadatak_12.c
@@ -0,0 +1,2 @@
+/* 20. Potrebno je napisati funkciju conf za konfiguraciju GPIOB void conf(int c). Ako c== 0 funkcija ne radi ništa. Ako c==1 pinovi 0-7 trebaju biti ulazni. 
+Ako c==1 pinovi 0-7 trebaju biti izlazni s dva logička stanja koja se mogu mijenjati najvećom brzinom. Inače pinovi 8-11 trebaju biti izlazni s otvorenim odvodom i priteznim tpornikom prema napajanju. */
