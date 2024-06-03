96ad8c3615fa35b0b72f046c1ec9ed5f606a0158
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-03 17:28:31 +0200

    Create zadatak_11.c

---
+++ zadatak_11.c
@@ -0,0 +1,2 @@
+/* 18. Zahtjevi za prekid pristižu od GPIOD1, GPIOD2 na padajući brid. Na prekid GPIOD1 potrebno je isključiti, a na prekid GPIOD2 uključiti motor koji je spojen na GPIOE11. 
+Prekidima je potrebno dodijeliti prioritet 4. Pretpostavite da je PRIGROUP=0. Inicijalizaciju ulazno-izlaznih jedinica i prekida potrebno je napraviti unutar main-a. */
