f7eaa8ded8ad97277dd461896041c48b84f2ec62
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-03 17:25:40 +0200

    Create zadatak_6.c

---
+++ zadatak_6.c
@@ -0,0 +1,5 @@
+/* 11. Napisati program koji u  čita 8-bitne vrijednosti s priključaka GPIOD0 - GPIOD7 te ih ispisuje na priključke GPIOD8 - GPIOD15 na slijedeći način: 
+ako je pročitana vrijednost parna uvećati je za jedan, a ako je neparna vrijednost ostaviti ju nepromijenjenom. 
+Program se mora izvršavati na sklopovlju mikrokontrolera STM32F407G. Za čitanje i ispis potrebno je koristiti funkcije scanf() i printf(). 
+Potrebno je koristiti redefiniranje ulazno-izlaznih funkcija koje posredno koriste semihost pozive.
+Pretpostaviti da je inicijalizacija sučelja Flash memorije i takta već napravljena. Funkcija za inicijalizaciju priključaka dana je i zove se init_GPIOD(). */
