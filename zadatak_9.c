59642614c1d567728ebc06cfa6a511e79d0cb2dd
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-03 17:27:33 +0200

    Create zadatak_9.c

---
+++ zadatak_9.c
@@ -0,0 +1,4 @@
+/* 16. Uz pretpostavku da AHB djelilo (AHB prescaler) na svom izlazu daje takt najviše dopuštene frekvencije, 
+potrebno je napisati funkciju za inicijalizaciju sistemskog brojila i pripadajućeg prekida tako da do prekida dolazi s periodom od 1 ms. 
+Nadalje, napisati prekidnu funkciju koja poslužuje ovaj prekid. Ova funkcija povećava sadržaj globalnih 32-bitnih nepredznačenih varijabli V1 i V2. 
+Stanje varijable V1 potrebno je povećati u svakom pozivu prekidne funkcije, a stanje varijable V2 u svakom desetom pozivu. */
