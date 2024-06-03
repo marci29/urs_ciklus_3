dffa8f640c61872efdad88d7ac62d2a31faa9004
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-03 17:29:59 +0200

    Create zadatak_14.c

---
+++ zadatak_14.c
@@ -0,0 +1,2 @@
+/* 22. Uz pretpostavku da AHB prescaler na svom izlazu daje takt najviše dopuštene frekvencije, napisati funkciju za inicijalizaciju sistemskog brojila I pripadajućeg prekida tako da prekid dolazi sa
+periodom 1ms. Napisati prekidnu funkciju koja poslužuje ovaj prekid. Ova funkcija povećava sadržaj globalnih nepredznačenih varijabli V1 I V2. V1 povećati u svakom pozivu, V2 u svakom 10. pozivu. */
