16d0a9276c2ef5482b8008fc38f91e80cc40e320
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-03 17:29:33 +0200

    Create zadatak_13.c

---
+++ zadatak_13.c
@@ -0,0 +1,2 @@
+/* 21. Napisati program koji u petlji čita s GPIOA0-GPIOA7 I ispisuje ih na GPIOA8-GPIOA15 na način: ako je pročitan parni broj dodaj mu jedan I ispiši, ako je pročitan neparni samo ga ispiši. 
+Za čitanje i ispis potrebno je koristiti scanf() I printf() (redefiniranjem). Inicijalizacija je već napravljena pomoću funkcije init_GPIOA(). */
