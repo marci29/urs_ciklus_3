4951cefc1bf590c82a1fa9e2edaba84346f080b6
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-12 18:37:10 +0200

    Update zadatak_17.c

--- zadatak_17.c
+++ zadatak_17.c
@@ -1,3 +1,5 @@
+// jednak kao 7.
+
 /* 25. Napisati programsku podršku za obradu prekida čiji zahtjevi dolaze od dviju vanjskih jedinica spojenih na priključke GPIOD1 i GPIOD2. 
 Jedinice na svojim izlazima daju digitalne signale s dva stanja, a zahtjev za prekid označava padajući brid. Prekid prve jedinice mora uključiti, a prekid druge jedinice isključiti motor spojen na GPIOE11. 
 Prekidima dodijeliti prioritet 4. Pretpostaviti da je inicijalizacija Flash kontrolera i izvora takta već napravljena. Također pretpostaviti da je grupiranje prekida već napravljeno te da je PRIGROUP = 0. 
