c4d50db7c0a2988594c8eac85445f8e1be58b424
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-12 18:46:50 +0200

    Update zadatak_5.c

--- zadatak_5.c
+++ zadatak_5.c
@@ -1,6 +1,6 @@
 // isti kao 16.
 
-/* Napisati programsku podršku za obradu prekida čiji zahtjevi dolaze od vanjske jedinice spojene na priključak GPIOD3. 
+/* 9. Napisati programsku podršku za obradu prekida čiji zahtjevi dolaze od vanjske jedinice spojene na priključak GPIOD3. 
 Jedinica generira digitalni signal s dva stanja čiji su rastući bridovi jednako razmaknuti u vremenu za 10 ms. Prekidna funkcija mora na GPIOB14 generirati pravokutni signal frekvencije 2 Hz. 
 GPIO14 pogoni sporu vanjsku jedinicu čiji ulaz podržava dva logička stanja.
 Inicijalizaciju ulazno-izlaznog sklopovlja i prekida napraviti u okviru funkcije main(). Prekidu dodijeliti prioritet 2. 
