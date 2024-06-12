4669d2ed405bbc39e4f5c0ba6648a8154244ed46
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-12 18:40:10 +0200

    Update zadatak_10.c

--- zadatak_10.c
+++ zadatak_10.c
@@ -1,1 +1,5 @@
-/* 17. Napišite program koji na serijsko sučelje jednom ispiše sve karaktere 8-bitne ASCII tablice, koristiti USART2 i redefiniciju funkcija koje posredno koriste semihosting. Za ispis koristiti printf(). */
+// isti kao https://github.com/marci29/urs_ciklus_3/blob/main/zadatak_4.c
+
+
+/* 17. Napišite program koji na serijsko sučelje jednom ispiše sve karaktere 8-bitne ASCII tablice,
+koristiti USART2 i redefiniciju funkcija koje posredno koriste semihosting. Za ispis koristiti printf(). */
