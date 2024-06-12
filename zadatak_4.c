4be6db87ef63b69f53ceac25407a635cb5a3039e
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-12 18:40:55 +0200

    Update zadatak_4.c

--- zadatak_4.c
+++ zadatak_4.c
@@ -1,3 +1,5 @@
+// isti kao https://github.com/marci29/urs_ciklus_3/blob/main/zadatak_10.c
+
 /* 8. Napisati program koji na serijsko sučelje jednom ispiše sve karaktere iz 8-bitne ASCII tablice. Treba ih ispisati redom bez obzira radi li se o alfanumeričkim ili specijalnim znakovima. 
 Program se mora izvršavati u sklopovlju STM32F407μC. Kao izlaznu jedinicu koristiti redefiniranje ulazno-izlaznih funkcija koje neposredno koriste semihosting.
 Pretpostavite da je zadana funkcija za inicijalizaciju sklopa USART2 i pripadajućih GPIO priključaka (init_USART2). 
