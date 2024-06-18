ed5dff52d47cb588c950687b5a91b11853cf397e
marci29 <50134254+marci29@users.noreply.github.com>
2024-06-18 17:09:14 +0200

    Update zadatak_3.c

--- zadatak_3.c
+++ zadatak_3.c
@@ -1,4 +1,6 @@
-/* 3. Uz pretpostavku da AHB djelilo (AHB prescaler) na svom izlazu daje takt frekvencije f = 42 MHz, potrebno je napisati funkciju za inicijalizaciju sistemskog brojila i pripadajućeg prekida 
+/* 3. Uz pretpostavku da AHB djelilo (AHB prescaler) na svom izlazu daje takt frekvencije f = 42 MHz, 
+potrebno je napisati funkciju za inicijalizaciju sistemskog brojila i pripadajućeg prekida 
 tako da do prekida dolazi s periodom od 1 ms. Nadalje, napisati prekidnu funkciju koja poslužuje ovaj prekid. 
-Ova funkcija povećava sadržaj globalne 32-bitne nepredznačene varijable V1. Korištenjem varijable V1 napraviti funkciju za generiranje pauza void Pause(uint32_t NumberOfMiliseconds); 
+Ova funkcija povećava sadržaj globalne 32-bitne nepredznačene varijable V1. 
+Korištenjem varijable V1 napraviti funkciju za generiranje pauza void Pause(uint32_t NumberOfMiliseconds); 
 Napisati program koji izmjenično pali i gasi diodu na PD15 s periodom 2 s. */
